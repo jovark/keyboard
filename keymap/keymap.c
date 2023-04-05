a2f0a84926abe4455fd9c1a11aa064c1df3c2e05
Joonatan <joonatanvark@gmail.com>
2023-04-05 19:29:23 +0300

    moved '' key

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL,
         KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
-                         KC_LGUI,KC_GRV,                                                       KC_LBRC, KC_RBRC,
+                         KC_GRV,_______,                                                       KC_LBRC, KC_RBRC,
                                          RAISE,KC_SPC,                         KC_BSPC, KC_TAB,
                                          KC_ENT,KC_LGUI,                       KC_LBRC,  KC_LALT,
                                          KC_CAPS, KC_LALT,                        KC_RBRC, KC_DEL
