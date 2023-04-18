125672d5d92af3e80b46066526ba9aa72a40cc4f
Joonatan <joonatanvark@gmail.com>
2023-04-18 17:28:10 +0300

    v1.3

--- keymap/config.h
+++ keymap/config.h
@@ -18,9 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define MASTER_LEFT
-//#define MASTER_RIGHT
-
-//#define EE_HANDS
 
 /* Fix other half */
 #define SPLIT_USB_DETECT
